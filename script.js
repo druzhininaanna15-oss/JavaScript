740fcd3fbea6a6965c79a4e592c7639fef9757a1
Anna <druzhininaanna15@gmail.com>
2025-12-06 14:34:01 +0300

    creat project

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = "Аня";
+let age = 15;
+let city = "Дальнереченск";
+
+let userdata = `имя пользователя: ${name}, возраст: ${age}, город: ${city}`;
+console.log(userdata)
+
+
+let a = 10;
+let b = 5;
+
+let resuls = `Результаты операций:
+a = ${a}, b = ${b}
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}`;
+
+console.log(resuls)
